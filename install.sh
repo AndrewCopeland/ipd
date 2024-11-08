4dc294c65595cb18f31d0371f1e124ecf59bd265
AndrewCopeland <andcope1995@gmail.com>
2024-11-08 15:55:55 -0500

    update ./install.sh

--- install.sh
+++ install.sh
@@ -7,4 +7,7 @@ fileName="ipd_${os}_${architecture}.tar.gz"
 url="https://raw.githubusercontent.com/AndrewCopeland/ipd/refs/heads/main/dist/$fileName"
 
 echo "downloading ipd from $url"
-
+curl -O "$url"
+tar -xvzf "$fileName"
+sudo mv "./ipd" "/usr/local/bin/ipd"
+rm "$fileName"
